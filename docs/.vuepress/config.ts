aff1607c790910fb9f35623747108db3594e8f58
RiceCake404 <etenaelta@outlook.com>
2021-10-28 01:45:15 +0800

    feat(side-bar): add side bar setting

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,7 +6,27 @@ export default defineUserConfig<DefaultThemeOptions>({
     title: '鱼糕文档',
     description: '最终幻想FF14鱼糕文档站',
 
+
     themeConfig: {
         logo: 'https://cdn.jsdelivr.net/gh/ricecake404/images@main/img/pastry-fish.png',
+        sidebar: {
+            '/guide/': [
+                {
+                    text: '指南',
+                    children: [
+                        '/guide/README.md',
+                        '/guide/getting-started.md',
+                        '/guide/full-guide.md',
+                    ],
+                },
+
+            ]
+        }
     },
+
+    markdown: {
+        extractHeaders: {
+            level: [2, 3, 4]
+        }
+    }
 })
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,13 @@
-# Hello VuePress
+---
+home: true
+title: Home
+heroImage: https://cdn.jsdelivr.net/gh/ricecake404/images@main/img/pastry-fish.png
+actions:
+  - text: 新手入门
+    link: /guide/getting-started.html
+    type: primary
+  - text: 光之渔夫手册
+    link: /guide/full-guide.html
+    type: secondary
+footer: MIT Licensed | Copyright © 2021-present RiceCake
+---
