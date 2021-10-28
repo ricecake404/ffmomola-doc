3799d75a5d80ee0074b97b7797926e3ade9e16fa
RiceCake404 <etenaelta@outlook.com>
2021-10-28 15:20:34 +0800

    feat(edit): add edit link

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,32 +1,31 @@
-import { defineUserConfig } from 'vuepress'
-import type { DefaultThemeOptions } from 'vuepress'
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
 
 export default defineUserConfig<DefaultThemeOptions>({
-    lang: 'zh-CN',
-    title: '鱼糕文档',
-    description: '最终幻想FF14鱼糕文档站',
+  lang: "zh-CN",
+  title: "鱼糕文档",
+  description: "最终幻想FF14鱼糕文档站",
 
-
-    themeConfig: {
-        logo: 'https://cdn.jsdelivr.net/gh/ricecake404/images@main/img/pastry-fish.png',
-        sidebar: {
-            '/guide/': [
-                {
-                    text: '指南',
-                    children: [
-                        '/guide/README.md',
-                        '/guide/getting-started.md',
-                        '/guide/full-guide.md',
-                    ],
-                },
-
-            ]
-        }
+  themeConfig: {
+    logo: "https://cdn.jsdelivr.net/gh/ricecake404/images@main/img/pastry-fish.png",
+    repo: "https://ricecake302.coding.net/p/pastry-fish/d/pastry-fish-doc/git",
+    repoLabel: "文档项目",
+    docsRepo:
+      "https://ricecake302.coding.net/p/pastry-fish/d/pastry-fish-doc/git",
+    docsBranch: "master",
+    docsDir: "docs",
+    editLinkPattern: ":repo/tree/:branch/:path?tab=file",
+    sidebar: {
+      "/guide/": [
+        {
+          text: "指南",
+          children: [
+            "/guide/README.md",
+            "/guide/getting-started.md",
+            "/guide/full-guide.md",
+          ],
+        },
+      ],
     },
-
-    markdown: {
-        extractHeaders: {
-            level: [2, 3, 4]
-        }
-    }
-})
+  },
+});
