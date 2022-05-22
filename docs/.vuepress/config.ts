8ed30aa02ec4f5850e2f05e0cc818ae5fe4b8e15
RiceCake404 <73119993+ricecake404@users.noreply.github.com>
2022-05-22 09:56:22 +0000

    feat: editLinkText

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,7 +5,6 @@ export default defineUserConfig<DefaultThemeOptions>({
   lang: "zh-CN",
   title: "鱼糕文档",
   description: "最终幻想FF14鱼糕文档站",
-
   themeConfig: {
     logo: "https://static.ffmomola.com/pastry-fish-static-files/img/logo/pastry-fish.webp",
     repo: "ricecake404/ffmomola-doc",
@@ -13,7 +12,7 @@ export default defineUserConfig<DefaultThemeOptions>({
     docsBranch: "master",
     docsDir: "docs",
     editLinkPattern: ":repo/tree/:branch/:path?tab=file",
-    editLinkText: "编辑此页",
+    editLinkText: "在Github上编辑此页",
     lastUpdatedText: "上次更新",
     contributorsText: "编辑",
     sidebar: {
